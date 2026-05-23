d7ee077d1d318ab74e77358715cf0ab0ef5925cc
Sayazhan <253479@astanait.edu.kz>
2026-05-23 15:53:21 +0600

    4assignment bonus

--- Vertex.java
+++ Vertex.java
@@ -1,15 +1,19 @@
 public class Vertex {
-    private int id;
+    private int id; // Уникальный идентификатор вершины
 
+    // Конструктор
     public Vertex(int id) {
         this.id = id;
     }
 
+    // Геттер для получения id
     public int getId() {
         return id;
     }
 
+    // Метод для вывода вершины в виде строки
+    @Override
     public String toString() {
-        return "" + id;
+        return String.valueOf(id);
     }
 }
