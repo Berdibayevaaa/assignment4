b9ac02316f50717cdbd33b5460d7ed8296208815
Sayazhan <253479@astanait.edu.kz>
2026-05-23 15:59:51 +0600

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Experiment experiment = new Experiment();
-
-        // Запуск всех тестов (включая BFS, DFS и Дейкстру)
-        experiment.runMultipleTests();
-        experiment.printResults();
-    }
-}
